ae71798bdac15635062239ed33785b2fc9c9cabc
unknown <torres@alunos.utfpr.edu.br>
2020-08-24 02:37:08 -0300

    ...4

--- express.js
+++ express.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const favicon = require('express-favicon');
+const bodyParser = require('body-parser');
 const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.post('/api/world', (req, res) => {
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
 });
-
-app.listen(port);
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+app.listen(port, () => console.log(`Listening on port ${port}`));
